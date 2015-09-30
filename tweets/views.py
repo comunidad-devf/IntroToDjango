af6769178b1041390e742f771d0af33ac61ecd7a
Pablo Trinidad <pabloatp0204@gmail.com>
2015-09-30 16:09:44 -0500

    View and Template logic

--- tweets/views.py
+++ tweets/views.py
@@ -1,9 +1,12 @@
 """Tweets views."""
 
 from django.shortcuts import render
+from models import Tweet
 
 
 def home(request):
     """Home View for / URL."""
-    context = {}
+    context = {
+        'tweets': Tweet.objects.all()
+    }
     return render(request, 'index.html', context)
